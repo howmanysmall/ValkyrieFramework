efbce2b341c2f04588e81e2452a63d3035ea1ebb
gskw <agoldstar101@gmail.com>
2015-07-13 14:34:00 +0000

    lib/check_cokey lapis.db experiment

--- lib/check_cokey.lua
+++ lib/check_cokey.lua
@@ -1,27 +1,27 @@
 local module      = {};
-local mysql       = library("mysql");
+local mysql       = require"lapis.db";
 local encoder     = library("encode");
 local app_helpers = require"lapis.application";
 
 local yield_error = app_helpers.yield_error;
 
 function module.check(gid, cokey, uid)
-  local game_id_result  = mysql.query(mysql.select_base, "id", "game_ids", ("gid='%s' AND cokey='%s'"):format(mysql.safe(gid, cokey)));
-  if game_id_result:numrows() == 0 then
+  local game_id_result  = mysql.select("id FROM game_ids WHERE gid=? AND cokey=?", gid, cokey);
+  if #game_id_result == 0 then
     yield_error("Invalid UID-GID-CoKey combination!");
   end
 
-  local user_id_result = mysql.query(mysql.select_base, "id", ("`trusted_users_%s`"):format(mysql.safe(gid)), ("uid='%d' AND connection_key='%s'"):format(mysql.safe(uid, cokey)));
-  if user_id_result:numrows() == 0 then
+  local user_id_result  = mysql.select("id FROM `trusted_users_?`, uid=? AND connection_key=?", gid, uid, cokey);
+  if #user_id_result == 0 then
     yield_error("Invalid UID-CoKey-GID combination!");
   end
 
-  return encoder.encode({success = true, error = ""});
+  return encoder.encode {success = true, error = ""};
 end
 
 function module.check_nouid(gid, cokey)
-  local game_id_result  = mysql.query(mysql.select_base, "id", "game_ids", ("gid='%s' AND cokey='%s'"):format(mysql.safe(gid, cokey)));
-  if game_id_result:numrows() == 0 then
+  local game_id_result  = mysql.select("id FROM game_ids WHERE gid=? AND cokey=?", gid, cokey);
+  if #game_id_result == 0 then
     yield_error("Invalid GID-CoKey pair!");
   end
 
