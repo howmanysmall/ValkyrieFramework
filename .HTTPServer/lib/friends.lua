d3747bf56aec0a0f29e3c7b5cbfaf17564d279e2
GSKW <agoldstar101@gmail.com>
2015-09-05 17:50:48 +0000

    Port friends.lua to lapis-mysql

--- .HTTPServer/lib/friends.lua
+++ .HTTPServer/lib/friends.lua
@@ -1,5 +1,5 @@
 local module          = {};
-local mysql           = library("mysql");
+local mysql           = require"lapis.db";
 local encoder         = library("encode");
 local app_helpers     = require"lapis.application";
 local http            = require("socket.http");
@@ -10,19 +10,13 @@ local yield_error     = app_helpers.yield_error;
 
 function module.getFriends(id)
   local ret           = {};
-  local ingamep_ret   = mysql.query(mysql.select_base, "gid, player", "player_ingame", "1=1");
+  local ingamep_ret   = mysql.select("gid, player from player_ingame");
   local friends       = http.request(("http://api.roblox.com/users/%d/friends"):format(id));
   friends             = json.decode(friends);
-  local ingameplayers = {};
-  local row           = ingamep_ret:fetch({}, "a");
-  while row do
-    ingameplayers[tonumber(row.player)] = row.gid;
-    row               = ingamep_ret:fetch({}, "a");
-  end
 
   for index, value in next, friends do
-    local toinsert    = {value.Id; value.Username; ingameplayers[value.Id] and true or false};
-    table.insert(toinsert, ingameplayers[value.Id]);
+    local toinsert    = {value.Id; value.Username; ingamep_ret[value.Id] and true or false};
+    table.insert(toinsert, ingamep_ret[value.Id]);
     table.insert(ret, toinsert);
   end
 
@@ -30,23 +24,42 @@ function module.getFriends(id)
 end
 
 function module.setOnlineGame(id, gid)
-  local exists_ret    = mysql.query(mysql.select_base, "gid", "player_ingame", ("player=%d"):format(id));
-  if exists_ret:numrows() == 0 then
-    mysql.query(mysql.insert_base, "player_ingame", ("player=%d, gid='%s'"):format(id, mysql.safe(gid)));
+  local exists_ret    = mysql.select("gid from player_ingame where player=?", id);
+  if #exists_ret < 1 then
+    mysql.insert("player_ingame", {
+      player          = id;
+      gid             = gid;
+    });
   else
-    mysql.query(mysql.update_base, "player_ingame", ("player=%d, gid='%s'"):format(id, mysql.safe(gid)), ("player=%d"):format(id));
+    mysql.update("player_ingame", {
+      player           = id;
+      gid              = gid;
+    }, {
+      player           = id;
+    }); 
   end
 
   userinfo.tryCreateUser(id);
 
-  mysql.query(mysql.update_base, "player_info", "last_online=0", ("player=%d"):format(id));
+  mysql.update("player_info", {
+    last_online      = 0;
+  }, {
+    player           = id;
+  });
 
   return encoder.encode({success = true, error = ""});
 end
 
 function module.goOffline(id, time_ingame)
-  mysql.query(mysql.delete_base, "player_ingame", ("player=%d"):format(id));
-  mysql.query(mysql.update_base, "player_info", ("last_online=%d, time_ingame=time_ingame+%d"):format(math.floor(socket.gettime()), time_ingame), ("player=%d"):format(id));
+  mysql.delete("player_ingame", {
+    player           = id;
+  });
+  mysql.update("player_info", {
+    last_online      = math.floor(socket.gettime());
+    time_ingame      = mysql.raw(("time_ingame+%d"):format(time_ingame));
+  }, {
+    player           = id;
+  });
 
   return encoder.encode({success = true, error = ""})
 end
