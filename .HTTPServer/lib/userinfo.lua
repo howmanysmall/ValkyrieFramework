7e0456a1fd0e829e0efd13cde6dcc428f20e3d0d
GSKW <agoldstar101@gmail.com>
2015-09-06 07:55:35 +0000

    Port userinfo.lua to lapis-mysql

--- .HTTPServer/lib/userinfo.lua
+++ .HTTPServer/lib/userinfo.lua
@@ -1,5 +1,5 @@
 local module                = {};
-local mysql                 = library("mysql");
+local mysql                 = require"lapis.db";
 local encoder               = library("encode");
 local parser                = library("parse");
 local app_helpers           = require"lapis.application";
@@ -9,19 +9,18 @@ local socket                = require("socket");
 local json                  = require("cjson");
 local meta                  = library("meta");
 local friends               = {};
+local gid_table             = library("gid_table");
 
 local yield_error           = app_helpers.yield_error;
 
 function friends.getFriends(id)
   local ret           = {};
-  local ingamep_ret   = mysql.query(mysql.select_base, "gid, player", "player_ingame", "1=1");
+  local ingamep_ret   = mysql.select("gid, player from player_ingame");
   local friends       = http.request(("http://api.roblox.com/users/%d/friends"):format(id));
   friends             = json.decode(friends);
   local ingameplayers = {};
-  local row           = ingamep_ret:fetch({}, "a");
-  while row do
-    ingameplayers[tonumber(row.player)] = row.gid;
-    row               = ingamep_ret:fetch({}, "a");
+  for i = 1, #ingamep_ret do
+    ingameplayers[tonumber(ingamep_ret[i].player)] = ingamep_ret[i].gid;
   end
 
   for index, value in next, friends do
@@ -35,17 +34,17 @@ end
 
 local function getGeneralInfo(id)
   local ret                 = {};
-  local ingamep_ret         = mysql.query(mysql.select_base, "gid, player", "player_ingame", ("player=%d"):format(id));
+  local ingamep_ret         = mysql.select("gid, player from player_ingame where player=?", id);
   local otherinfo           = http.request(("http://api.roblox.com/users/%d"):format(id));
   otherinfo                 = json.decode(otherinfo);
-  local row                 = ingamep_ret:fetch({}, "a");
+  local online              = #ingamep_ret > 0;
   local gamename            = nil;
 
-  if row then
-    gamename                = meta.getMeta("name", row.gid);
+  if online then
+    gamename                = meta.getMeta("name", ingamep_ret[1].gid);
   end
 
-  return {otherinfo.Id; otherinfo.Username; row and true or false; row and row.gid or nil; gamename};
+  return {otherinfo.Id; otherinfo.Username; online and true or false; online and ingamep_ret[1].gid or nil; gamename};
 end
 
 local function fixDate(date, diff)
@@ -70,9 +69,9 @@ end
 
 local function getDBGeneralInfo(id)
   local ret                 = {{}, {}};
-  local userinfo_ret        = mysql.query(mysql.select_base, "*", "player_info", ("player=%d"):format(id));
+  local userinfo_ret        = mysql.select("* from player_info where player=?", id);
 
-  local row                 = userinfo_ret:fetch({}, "a");
+  local row                 = userinfo_ret[1];
   if row then
     ret[2]                  = {row.time_ingame, row.joined, row.last_online};
     table.insert(ret[1], fixDate(os.date("%Ya %mm %dd %Hh %Mmin", row.time_ingame), true));
@@ -86,9 +85,14 @@ local function getDBGeneralInfo(id)
 end
 
 function module.tryCreateUser(id)
-  local exists_ret          = mysql.query(mysql.select_base, "id", "player_info", ("player=%d"):format(id));
-  if exists_ret:numrows() == 0 then
-    mysql.query(mysql.insert_base, "player_info", ("player=%d, time_ingame=0, joined=%d, last_online=0"):format(id, math.floor(socket.gettime())));
+  local exists_ret          = mysql.select("id from player_info where player=?", id);
+  if #exists_ret < 1 then
+    mysql.insert("player_info", {
+      player                = id;
+      time_ingame           = 0;
+      joined                = math.floor(socket.gettime());
+      last_online           = 0;
+    });
   end
 
   return encoder.encode({success = true; error = ""});
@@ -102,20 +106,17 @@ function module.getUserinfo(id)
   table.insert(ret, getGeneralInfo(id));
   table.insert(ret, getDBGeneralInfo(id));
 
-  local exists_result       = mysql.query(mysql.select_base, "table_name", "information_schema.tables", ("table_name='player_achv_%d'"):format(id));
-  if exists_result:numrows() ~= 0 then
-    local achvs_result      = mysql.query(mysql.select_base, "*", ("player_achv_%d"):format(id), "1=1");
-    local row               = achvs_result:fetch({}, "a");
+  local exists_result       = mysql.select("table_name from information_schema.tables where table_name=?", ("player_achv_%d"):format(id));
+  if #exists_result > 0 then
+    local achvs_result      = mysql.select("* from ?", gid_table("player_achv", id));
     local checkedGames      = {};
     local insertret         = {};
-    while row do
+    for _, row in next, achvs_result do
       if checkedGames[row.gid] == nil then
-        local game_achv_res   = mysql.query(mysql.select_base, "*", ("achievements_%s"):format(row.gid), "1=1");
+        local game_achv_res   = mysql.select("* from ?", gid_table("achievements", row.gid));
         local insertretchk    = {};
-        local row2            = game_achv_res:fetch({}, "a");
-        while row2 do
+        for __, row2 in next, game_achv_res do
           insertretchk[row2.achv_id] = row2;
-          row2                = game_achv_res:fetch({}, "a");
         end
         checkedGames[row.gid] = insertretchk;
       end
@@ -128,7 +129,6 @@ function module.getUserinfo(id)
       table.insert(insinsret, tonumber(checkedGames[row.gid][row.achvid].icon));
       table.insert(insinsret, meta.getMeta("name", row.gid));
       insertret[row.achvid]   = insinsret;
-      row                     = achvs_result:fetch({}, "a");
     end
     table.insert(ret, insertret);
 
