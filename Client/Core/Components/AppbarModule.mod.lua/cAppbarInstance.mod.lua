194a3009468881aa14351ca29a62f550334ef9af
GSKW <agoldstar101@gmail.com>
2015-09-15 17:32:56 +0300

    Quickly fix the Appbar's X size

--- Client/Core/Components/AppbarModule.mod.lua/cAppbarInstance.mod.lua
+++ Client/Core/Components/AppbarModule.mod.lua/cAppbarInstance.mod.lua
@@ -176,7 +176,7 @@ function cAppbarInstance.new(Settings, Tween, Duration, Async)
 	LeftAltIcon.Position 					= UDim2.new(0, -16 - Height + 32, 0, 16);
 	RightAltIcon.Position					= UDim2.new(1,  16 + Height - 32, 0, 16);
 
-	Appbar.Size 							= UDim2.new(0, 1, 0, Height);
+	Appbar.Size 							= UDim2.new(1, 0, 0, Height);
 
 	Appbar.Parent 							= Core:GetOverlay();
 
