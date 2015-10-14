f8cf6c7f6b37d4fce66d4130420047565e652cfa
eLunate <exp.lunate@gmail.com>
2015-10-14 10:55:17 +0100

    Create ValkyrieInput.mod.lua
    * Classes/ValkyrieInput
    |~ Templated the input Component

---
+++ Client/ValkyrieInput.mod.lua
@@ -0,0 +1,21 @@
+-- Valkyrie Input
+
+-- Grab all input services
+local UIS = game:GetService("UserInputService");
+local CAS = game:GetService("ContextActionService");
+local Player = game.Players.LocalPlayer;
+while not Player do wait(); Player = game.Players.LocalPlayer; end;
+local Mouse = Player:GetMouse();
+
+-- Containers for internal data
+local InputFormats = {};
+
+-- Constructors for external types
+
+-- Capture events
+
+-- Create the Interface
+
+-- Build the module
+
+-- Return
