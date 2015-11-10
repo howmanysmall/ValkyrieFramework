3eddd2712feb4d788d0b19246aedf5aeeab40fb3
eLunate <exp.lunate@gmail.com>
2015-11-10 11:14:07 +0000

    Update ValkyrieInput.mod.lua
    update

--- Client/Core/Components/ValkyrieInput.mod.lua
+++ Client/Core/Components/ValkyrieInput.mod.lua
@@ -152,9 +152,12 @@ function Controller.CreateAction(...)
 	return newAction;
 end;
 
-function ActionClass:Unbind()
-	ActionBinds[self]:disconnect();
-	ActionBinds[self] = nil;
+function ActionClass:UnbindAll()
+	local binds = ActionBinds[self];
+	for i=#binds,1,-1 do
+		binds[i]:disconnect();
+		binds[i] = nil;
+	end;
 end;
 
 function ActionClass:SetFlag(flag, value)
@@ -169,12 +172,108 @@ function ActionClass:SetFlag(flag, value)
 		);
 	end;
 end;
-
-function ActionClass:Bind(source, dir, func)
+do
+	local UIS = game:GetService("UserInputService");
+	local Mouse = game:GetService("Players").LocalPlayer;
+	local CAS = game:GetService("ContextActionService");
+	
+	local CustomConnection do
+		-- Constructor for custom Connection objects
+		local finishers = setmetatable({},{__mode = 'k'});
+		local disconnectAction = function(self)
+			if finishers[self] then
+				finishers[self](self);
+				finishers[self] = nil;
+			else
+				warn("Unable to disconnect disconnected action for ValkyrieInput");
+			end;
+		end;
+		local cmt = {
+			__index = function(t,k)
+				if k == 'disconnect' then
+					return disconnectAction;
+				end;
+			end;
+			__metatable = "Locked metatable: Valkyrie";
+			__tostring = function()
+				return "Connection object for ValkyrieInput";
+			end;
+		};
+		CustomConnection = function(disconnectFunc)
+			local newConnection = newproxy(true);
+			local newMt = getmetatable(newConnection);
+			for e,m in next, cmt do
+				newMt[e] = m;
+			end;
+			finishers[newConnection] = disconnecFunc;
+			return newConnection;
+		end;
+	end;
 	-- @source: Valkyrie Input type
 	-- @dir: Input direction (Up, Down, UpDown/Click)
 	-- @func: Binding function
-	
+	function ActionClass:BindControl(source, dir, func)
+		-- ~ UIS/Mouse style input sources to bind from
+		assert(source, "You need to supply an Input source as #1", 2);
+		local Type, Name = LinkedTypes[source],LinkedNames[source];
+		assert(
+			Type and Name, 
+			"You need to supply a valid Valkyrie Input as #1, did you supply a string by accident?",
+			2
+		);
+		if Type == 'Keyboard' then
+			
+		elseif Type == 'Mouse1' then
+			
+		elseif Type == 'Mouse2' then
+			
+		elseif Type == 'MouseMoved' then
+			
+		elseif Type == 'MouseScrolled' then
+			
+		elseif Type == 'ControllerButton' then
+			
+		elseif Type == 'TouchScreen' then
+			
+		end;
+		
+		--> Connection
+	end;
+	function ActionClass:BindSource(source, dir, object, func)
+		-- ~ Binding actions for Instances with input sources
+		-- @object: Binding target
+		
+		--> Connection
+	end;
+	function ActionClass:BindContext(keyboard, controller, touchscreen, dir, makebutton, func)
+		-- ~ Extra content binding, CAS style
+		-- @keyboard: Keyboard input type
+		-- @controller: Controller input type
+		-- @touchscreen: Touchscreen input type
+		-- @dir: ::@dir
+		-- @makebutton: Create an onscreen button for this input source?
+		
+		--> Connection, ?Button
+	end;
+	function ActionClass:CreateButton(source, dir, name, color, position, func)
+		-- @name: Name of the button, and the text displayed. Can be nil.
+		-- @color: Color3 of the button. Can be nil.
+		-- | Color can also be a name of a Material Color for a [500] Color
+		-- @position: UDim2 of the button Position. Can be nil (automatic positioning)
+		
+		--> Connection, Button
+	end;
+	function ActionClass:BindCombo(sources, func)
+		-- @sources: Table array of Valkyrie Input Sources
+		-- | Sources are to be checked with a DownUp/Click
+		
+		--> Connection
+	end;
+	function ActionClass:BindSequence(sources, func)
+		-- @sources: Table array of Valkyrie Input Sources
+		-- | Sources are to be checked in order. No tree building here.
+		
+		--> Connection
 end;
 
 
