2cc536837fbf6615f7496b23b9164755961cd444
eLunate <exp.lunate@gmail.com>
2015-12-29 19:59:00 +0000

    Take #2

--- Client/Loader.loc.lua
+++ Client/Loader.loc.lua
@@ -1,11 +1,7 @@
 local Player = game.Players.LocalPlayer;
 local c = Player.Character or Player.CharacterAdded:wait();
 print("Loading Valkyrie Client");
-require(script.Parent:WaitForChild("ValkyrieClient").Valkyrie);
 wait();
-local ch = script.Parent.ValkyrieClient:GetChildren();
 script.Parent.ValkyrieClient.Parent = Player:WaitForChild("PlayerScripts")
-for i=1,#ch do
-	ch[i].Parent = Player.PlayerScripts.ValkyrieClient;
-end
+require(script.Parent:WaitForChild("ValkyrieClient").Valkyrie);
 print("Loaded Valkyrie Client");
