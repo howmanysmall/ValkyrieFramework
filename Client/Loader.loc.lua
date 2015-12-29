6f1e001b6807c8182991c5380be7eb9517f28025
eLunate <exp.lunate@gmail.com>
2015-12-29 19:41:27 +0000

    Fix a loader location issue

--- Client/Loader.loc.lua
+++ Client/Loader.loc.lua
@@ -1,6 +1,6 @@
 local c = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:wait();
 print("Loading Valkyrie Client");
-require(script.Parent:WaitForChild("Valkyrie"));
+require(script.Parent:WaitForChild("ValkyrieClient").Valkyrie);
 wait();
 script.Parent.ValkyrieClient.Parent = game.Players.LocalPlayer:WaitForChild("PlayerScripts")
 print("Loaded Valkyrie Client");
