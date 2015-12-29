792d1340f4b33f12b950220f71fa510f4a73430b
eLunate <exp.lunate@gmail.com>
2015-12-29 19:56:01 +0000

    Attempt to reparent missing children.

--- Client/Loader.loc.lua
+++ Client/Loader.loc.lua
@@ -1,6 +1,11 @@
-local c = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:wait();
+local Player = game.Players.LocalPlayer;
+local c = Player.Character or Player.CharacterAdded:wait();
 print("Loading Valkyrie Client");
 require(script.Parent:WaitForChild("ValkyrieClient").Valkyrie);
 wait();
-script.Parent.ValkyrieClient.Parent = game.Players.LocalPlayer:WaitForChild("PlayerScripts")
+local ch = script.Parent.ValkyrieClient:GetChildren();
+script.Parent.ValkyrieClient.Parent = Player:WaitForChild("PlayerScripts")
+for i=1,#ch do
+	ch[i].Parent = Player.PlayerScripts.ValkyrieClient;
+end
 print("Loaded Valkyrie Client");
