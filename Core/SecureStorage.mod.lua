1ed8c47b70f476f7015da4f7251424ef9324c3ba
eLunate <exp.lunate@gmail.com>
2016-03-22 10:34:29 +0000

    Update SecureStorage - Deprecate later.

--- Core/SecureStorage.mod.lua
+++ Core/SecureStorage.mod.lua
@@ -3,7 +3,7 @@ local e = newproxy(true);
 local mt = getmetatable(e);
 mt.__index = storage;
 mt.__newindex = function(t,k,v) if storage[k] then storage[k] = v else error("You should not be setting "..tostring(k), 2) end end;
-mt.__metatable = "Secure storage ain't being touched";
+mt.__metatable = "Locked Metatable: Valkyrie";
 
 storage.Key = "";
 
