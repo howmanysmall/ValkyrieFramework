d3ea3e72a0e8f310051fba8a39b0fdbe2ca6c643
Skye (eLunate) <exp.lunate@gmail.com>
2016-05-13 23:59:38 +0100

    Remove SecureStorage
    Implications may need cleaning up later.

--- _.mod.lua
+++ _.mod.lua
@@ -228,8 +228,7 @@ vmt.__call = function(_, GID, CoKey)
 	assert(type(GID) ~= 'table' and type(GID) ~= 'userdata' and type(CoKey) ~= 'table' and type(CoKey) ~= 'userdata',
 		"You should not be passing a table or userdata, silly",2);
 	GId = GID;
-	require(script.Core.SecureStorage).Key = CoKey;
-    remoteComm.GiveDependencies(GID, URL, CoKey, ocxi.GetComponent "RequestEncode", ocxi.GetComponent "RequestDecode");
+  remoteComm.GiveDependencies(GID, URL, CoKey, ocxi.GetComponent "RequestEncode", ocxi.GetComponent "RequestDecode");
 	local resp
 	if not game:GetService("RunService"):IsStudio() then
 		resp = remoteComm.auth:check({uid = UId});
@@ -242,7 +241,7 @@ vmt.__call = function(_, GID, CoKey)
 	else
 		return error("Valkyrie Auth failure!");
 	end;
-	
+
 	local vc = script.Client:Clone();
 	vc.Name = "ValkyrieClient";
 	for _,v in ipairs(repSpace.Core:GetChildren()) do
@@ -269,7 +268,7 @@ vmt.__call = function(_, GID, CoKey)
 		end;
 	end;
 	ValkyrieSSS.Parent = game.ServerScriptService;
-	
+
 	local playerHandler = function(p)
 		local np = script.Server.Template:Clone();
 		np.Player.Value = p;
@@ -295,7 +294,7 @@ vmt.__call = function(_, GID, CoKey)
 		end
 		gpn.Parent = game.ReplicatedStorage;
 	end
-	
+
 	if not run:IsStudio() then
 		-- Load up the Valkyrie Player tracker.
 		require(342249737)(tostring(GID),tostring(CoKey));
