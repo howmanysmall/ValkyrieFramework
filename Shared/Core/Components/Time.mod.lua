66a650c83745a275c024e00f94b0106ce18a5530
Narrev <NarrevRBX@gmail.com>
2016-07-02 18:36:03 -0500

    Fixed other time module

--- Shared/Core/Components/Time.mod.lua
+++ Shared/Core/Components/Time.mod.lua
@@ -10,81 +10,56 @@ local TYEAR = 365.24*DAY;
 local MONTH = TYEAR/12;
 local iMONTH = DAY/0.0328549;
 
-local ce, fl, t, extract = math.ceil, math.floor, os.time
+local ceil, floor, t, format, extract = math.ceil, math.floor, os.time, string.format
 
-local Days = {
-    "Monday";
-    "Tuesday";
-    "Wednesday";
-    "Thursday";
-    "Friday";
-    "Saturday";
-    "Sunday"
-}
-local Months = {
-    31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-}
-local iMonths = {
-    "January";
-    "Febuary";
-    "March";
-    "April";
-    "May";
-    "June";
-    "July";
-    "August";
-    "September";
-    "October";
-    "November";
-}
+local Days = {[0] = "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+local iMonths = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
 
-local function isLeapYear(year)
-    if year%100 == 0 then
-        return year%400==0;
-    end;
-    return year%4==0;
-end
 local function TimeFromSeconds(...)
-    sec = extract(...) or t();
-    sec = sec%DAY
-    return string.format("%.2d:%.2d:%.2d", sec/HOUR, sec/60%60, sec%MINUTE)
+    local sec = extract(...) or t();
+    return format("%02d:%02d:%02d", floor(sec / HOUR % 24), floor(sec / MINUTE % MINUTE), floor(sec % MINUTE))
 end
-local function DayFromSeconds(sec) -- Epoch was on a Thursday
-    sec = sec or t();
-    return Days[ce(((sec+3*DAY)%WEEK)/DAY)]
-end
-local function GetMonth(...) -- Not so lazy months
-    sec = fl(extract(...) or t());
-    sec = sec+1; -- Otherwise it treats midnight as being yesterday
-    local ileap = isLeapYear(fl(sec/YEAR)+1970);
-    while sec > (ileap and LYEAR or YEAR) do
-    	sec = sec - (ileap and LYEAR or YEAR)
-    	ileap = isLeapYear(fl(sec/YEAR)+1970);
-    end;
-    local days = ce(sec/DAY)
-    local month = 0;
-    local useLeapFeb = isLeapYear(fl(sec/YEAR+1970))
-    for i,v in pairs(Months) do
-        if i==2 and useLeapFeb then v=29 end;
-        if days>v then
-            days = days-v;
-        else
-            month = iMonths[i]
-            break
-        end
+
+local function TimeEquations(bool, ...)
+    local sec   = extract(...) or t()
+    local days  = floor(sec / DAY) + 719468
+    local year  = floor((days >= 0 and days or days - 146096) / 146097)
+    days        = (days - year * 146097)
+    local years = floor((days - floor(days/1460) + floor(days/36524) - floor(days/146096))/365)
+    days        = days - (365*years + floor(years/4) - floor(years/100))
+    local month = floor((5*days + 2)/153)
+    days        = days - floor((153*month + 2)/5) + 1
+    month       = month + (month < 10 and 3 or -9)
+    year        = years + year*400 + (month < 3 and 1 or 0)
+
+    if bool then
+        return year
+    else
+        return iMonths[month], days
     end
-    return month, days;
 end
+
+local function DayFromSeconds(...)
+    --- Finds weekday name
+    local sec = extract(...) or t()
+    return Days[(floor(sec / DAY) + 4) % 7]
+end
+
+local function GetMonth(...)
+    return TimeEquations(false, ...)
+end
+
 local function FullDate(...) -- Remember...
-    sec = extract(...) or t();
+    local sec = extract(...) or t();
+    
     local ret = {}
     ret.Time = TimeFromSeconds(sec)
-    ret.Month, ret.Date = GetMonth(sec)
+    ret.Month, ret.Date = TimeEquations(false, sec)
     ret.Day = DayFromSeconds(sec)
-    ret.Year = fl(sec/YEAR)+1970
-    ret.Second = sec%MINUTE
-    ret.Minute = fl(sec/60%60)
-    ret.Hour = fl(sec%DAY/HOUR)
+    ret.Year = TimeEquations(true, sec)
+    ret.Second = floor(sec % MINUTE)
+    ret.Minute = floor(sec / MINUTE % MINUTE)
+    ret.Hour = floor(sec / HOUR % 24)
     return ret;
 end
 
@@ -106,6 +81,7 @@ local Controller = {
     MONTH = MONTH;
     iMONTH = iMONTH
 }
+
 local _Controller = newproxy(true);
 local mt = getmetatable(_Controller);
 mt.__metatable = "Locked metatable: Valkyrie";
